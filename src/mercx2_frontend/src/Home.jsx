50db6de6f12bc446e4322b3c590a7814ae136ec3
JumanaNehad <jumana1903964@miuegypt.edu.eg>
2025-01-24 19:26:52 +0200

    frontend

--- src/mercx2_frontend/src/Home.jsx
+++ src/mercx2_frontend/src/Home.jsx
@@ -107,7 +107,87 @@ function Home() {
  
     <div>
       {/* <MyNavbar /> */}
+      
       <main className="min-h-screen bg-gray-900 text-white p-4">
+      <section className="bg-slate-800 text-white rounded-lg shadow p-4 m-4">
+
+        {/* <section className="bg-slate-800 bg-opacity-80 text-white rounded-lg shadow p-4 m-4"> */}
+         
+<section className="bg-slate-800 bg-opacity-80 rounded-lg shadow ">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">See what's new</span> 
+            <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
+        </a>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Bridging Real-World Finance and Blockchain Freedom</h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Tokenize mutual funds, bonds, and stocks. Trade seamlessly. Earn confidently.</p>
+   
+        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            Get Started
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </a>
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+              Learn More
+            </a>  
+        </div>
+       
+        </div> 
+    {/* </div> */}
+
+        </section>
+
+        {/* <section>
+  <h2>Transaction History</h2>
+  <ul>
+    {transactions.length > 0 ? (
+      transactions.map((tx, index) => (
+        <li key={index}>
+          <p>Type: {tx.kind || "N/A"}</p>
+
+          <p>
+            Amount: 
+            {tx.transfer?.[0]?.amount 
+              ? tx.transfer[0].amount.toString() // Accessing the first transfer's amount
+              : tx.mint?.[0]?.amount 
+              ? tx.mint[0].amount.toString() // Accessing the first mint's amount
+              : "N/A"}
+          </p>
+
+          <p>
+            Timestamp: 
+            {tx.timestamp 
+              ? new Date(Number(tx.timestamp) / 1000000).toLocaleString() 
+              : "N/A"}
+          </p>
+
+          <p>
+            From: 
+            {tx.transfer?.[0]?.from?.owner 
+              ? tx.transfer[0].from.owner.toText() 
+              : "N/A"}
+          </p>
+
+          <p>
+            To: 
+            {tx.transfer?.[0]?.to?.owner 
+              ? tx.transfer[0].to.owner.toText() 
+              : tx.mint?.[0]?.to?.owner 
+              ? tx.mint[0].to.owner.toText() 
+              : "N/A"}
+          </p>
+        </li>
+      ))
+    ) : (
+      <p>No transactions found.</p>
+    )}
+  </ul>
+</section>
+
+
+ */}
+ </section>
+   
         <section className="bg-slate-800 text-white rounded-lg shadow p-4 m-4">
           <h2 className="text-lg font-bold">Your Balance</h2>
           <p className="text-xl">{!isAuthenticated ? `0 ${tokenName}` : `${balance.toString()} ${tokenName}`}</p>
@@ -164,6 +244,7 @@ function Home() {
 
 
  */}
+ 
       </main>
 
     </div>
@@ -176,5 +257,6 @@ export default () => (
   <Home />
 
 );
+
 
 
--- src/mercx2_frontend/tailwind.config.js
+++ src/mercx2_frontend/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line if you have different file extensions
     ],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'gradient-to-r-indigo-500-700': 'linear-gradient(to right, #667eea, #4c51bf)', // from indigo-500 to indigo-700
+            'gradient-to-r-indigo-700-darker': 'linear-gradient(to right, #4c51bf, #434190)', // from indigo-700 to a darker indigo
+          }
+      },
     },
     plugins: [],
   };
