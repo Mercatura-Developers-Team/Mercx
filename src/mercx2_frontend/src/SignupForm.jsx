669a4c6d88c5952ea7834f743a08ed6dfb7e98e9
JumanaNehad <jumana1903964@miuegypt.edu.eg>
2025-02-11 18:47:05 +0200

    form+deleteUser

--- src/mercx2_frontend/src/SignupForm.jsx
+++ src/mercx2_frontend/src/SignupForm.jsx
@@ -67,14 +67,14 @@ const SignupForm = () => {
     return <p>Please connect your wallet to sign up.</p>;
   }
     return ( <> 
-     <div className="max-w-md mx-auto bg-gray-900 p-6 rounded shadow-md">
+     <div className="max-w-md mx-auto bg-gray-900 p-6 rounded shadow-md m-20">
       <h2 className="text-2xl font-bold text-white mb-4">Signup</h2>
 
       {error && <p className="text-red-500">{error}</p>}
 
-      <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
-        <div>
-          <label htmlFor="username" className="text-white">Username</label>
+      <form onSubmit={formik.handleSubmit} className="max-w-sm mx-auto">
+        <div >
+          <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
           <input
             id="username"
             name="username"
@@ -82,14 +82,21 @@ const SignupForm = () => {
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.username}
-            className="bg-gray-800 text-white border-gray-600"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           {formik.touched.username && formik.errors.username && (
             <p className="text-red-400 text-sm">{formik.errors.username}</p>
           )}
         </div>
 
-        <button type="submit" disabled={loading}>
+        <div className="flex items-start mb-5">
+    <div className="flex items-center h-5">
+      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+    </div>
+    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" disabled={loading}>
           {loading ? "loading": "Sign Up"}
         </button>
       </form>
